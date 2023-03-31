ed5c3dd1db3dbc5c8ca8798f50f01cb6f7e8d62a
lalodiazdiaz <eduardodiazdiaz93@gmail.com>
2023-03-30 18:46:19 -0600

    links contact

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import "./Navbar.css";
 import logo from "../../assets/logo.png";
+import Whats from "../../assets/whats.png";
+import insta from '../../assets/instaLogo.png'
 import { NavLink, Outlet } from "react-router-dom";
 import menu from '../../assets/menuIcon.png'
 function Navbar() {
@@ -27,14 +29,25 @@ function Navbar() {
         </div>
         <div className="containerOptions">
           <NavLink className="optionNAv" onClick={closeSidebar} to='/'>
-            <p>Postres</p>
+            <p className="options">Postres</p>
           </NavLink>
           <NavLink className="optionNAv" onClick={closeSidebar} to='/Comidas'>
-            <p>Comidas</p>
+            <p className="options">Comidas</p>
           </NavLink>
           <NavLink className="optionNAv" onClick={closeSidebar} to='Bebidas'>
-            <p>Bebidas</p>
+            <p className="options">Bebidas</p>
           </NavLink>
+        </div>
+
+        <div className="containerFooter">
+          <div className="contInfo">
+          <img src={Whats} alt="" className="whatsLogo" />
+            <a href="https://api.whatsapp.com/send?phone=4494931335" target="_blank" rel={"noreferrer"}>Pedidos: 499-493-13-35</a>
+          </div>
+          <div className="contInfo">
+          <img src={insta} alt="" className="whatsLogo" />
+            <a href="https://instagram.com/kawa.ppy?igshid=YmMyMTA2M2Y=" target="_blank" rel={"noreferrer"}>Intagram: Kawappy</a>
+          </div>
         </div>
       </div>
 
